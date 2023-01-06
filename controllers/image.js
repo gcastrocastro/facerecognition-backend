818cc6d58c116226c3a7006b95080f85daf46ede
gcastrocastro <gcastrocastro.25@gmail.com>
2023-01-06 03:24:56 -0700

    update clarifai grpc

--- controllers/image.js
+++ controllers/image.js
@@ -1,18 +1,52 @@
-const Clarifai = require('clarifai');
+const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key process.env.apiKey");
+
+// const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai. 
-const app = new Clarifai.App({
-    apiKey: process.env.apiKey
-});
-
+// const app = new Clarifai.App({
+//     apiKey: process.env.apiKey
+// });
 const handleApiCall = (req, res) => {
-  app.models
-    .predict('53e1df302c079b3db8a0a36033ed2d15', req.body.input)
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => res.status(400).json('unable to work with API'))
+stub.PostModelOutputs(
+  {
+      // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+      model_id: "53e1df302c079b3db8a0a36033ed2d15",
+      inputs: [{data: {image: {url: req.body.input}}}]
+  },
+  metadata,
+  (err, response) => {
+      if (err) {
+          console.log("Error: " + err);
+          return;
+      }
+
+      if (response.status.code !== 10000) {
+          console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+          return;
+      }
+
+      console.log("Predicted concepts, with confidence values:")
+      for (const c of response.outputs[0].data.concepts) {
+          console.log(c.name + ": " + c.value);
+      }
+      res.json(response)
+  }
+);
 }
+
+// const handleApiCall = (req, res) => {
+//   app.models
+//     .predict('53e1df302c079b3db8a0a36033ed2d15', req.body.input)
+//     .then(data => {
+//       res.json(data);
+//     })
+//     .catch(err => res.status(400).json('unable to work with API'))
+// }
 
 const handleImage = (req, res, db)=> {
     const { id } = req.body;
