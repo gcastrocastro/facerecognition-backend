5272ce12e5b9b3bcf89a55820b7fb25c89e82f35
gcastrocastro <gcastrocastro.25@gmail.com>
2023-01-07 17:46:24 -0700

    yes

--- controllers/image.js
+++ controllers/image.js
@@ -3,7 +3,7 @@ const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
 const stub = ClarifaiStub.grpc();
 
 const metadata = new grpc.Metadata();
-metadata.set("authorization", "Key process.env.apiKey");
+metadata.set("authorization", "Key 4cd4379a9d5f4d0d83e258446c340baa");
 
 // const Clarifai = require('clarifai');
 
@@ -11,33 +11,34 @@ metadata.set("authorization", "Key process.env.apiKey");
 // const app = new Clarifai.App({
 //     apiKey: process.env.apiKey
 // });
+
 const handleApiCall = (req, res) => {
-stub.PostModelOutputs(
-  {
-      // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
-      model_id: "53e1df302c079b3db8a0a36033ed2d15",
-      inputs: [{data: {image: {url: req.body.input}}}]
-  },
-  metadata,
-  (err, response) => {
-      if (err) {
-          console.log("Error: " + err);
-          return;
-      }
+  stub.PostModelOutputs(
+    {
+        // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+        model_id: "53e1df302c079b3db8a0a36033ed2d15",
+        inputs: [{data: {image: {url: req.body.input}}}]
+    },
+    metadata,
+    (err, response) => {
+        if (err) {
+            console.log("Error: " + err);
+            return;
+        }
 
-      if (response.status.code !== 10000) {
-          console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
-          return;
-      }
+        if (response.status.code !== 10000) {
+            console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+            return;
+        }
 
-      console.log("Predicted concepts, with confidence values:")
-      for (const c of response.outputs[0].data.concepts) {
-          console.log(c.name + ": " + c.value);
-      }
-      res.json(response)
-  }
-);
-}
+        console.log("Predicted concepts, with confidence values:")
+        for (const c of response.outputs[0].data.concepts) {
+            console.log(c.name + ": " + c.value);
+        }
+        res.json(response);
+    }
+  )
+};
 
 // const handleApiCall = (req, res) => {
 //   app.models
