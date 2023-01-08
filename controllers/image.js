7f613624194059258c5776b0b894cc916345402f
gcastrocastro <gcastrocastro.25@gmail.com>
2023-01-07 18:18:27 -0700

    reverting to old api version

--- controllers/image.js
+++ controllers/image.js
@@ -1,53 +1,52 @@
-const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+// const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
 
-const stub = ClarifaiStub.grpc();
+// const stub = ClarifaiStub.grpc();
 
-const metadata = new grpc.Metadata();
-metadata.set("authorization", "Key process.env.apiKey");
+// const metadata = new grpc.Metadata();
+// metadata.set("authorization", "Key process.env.apiKey");
 
-// const Clarifai = require('clarifai');
+const Clarifai = require('clarifai');
 
-//You must add your own API key here from Clarifai. 
-// const app = new Clarifai.App({
-//     apiKey: process.env.apiKey
-// });
-
-const handleApiCall = (req, res) => {
-  stub.PostModelOutputs(
-    {
-        // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
-        model_id: "5e026c5fae004ed4a83263ebaabec49e",
-        inputs: [{data: {image: {url: req.body.input}}}]
-    },
-    metadata,
-    (err, response) => {
-        if (err) {
-            console.log("Error: " + err);
-            return;
-        }
-
-        if (response.status.code !== 10000) {
-            console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
-            return;
-        }
-
-        console.log("Predicted concepts, with confidence values:")
-        for (const c of response.outputs[0].data.concepts) {
-            console.log(c.name + ": " + c.value);
-        }
-        res.json(response.outputs[0].data.concepts);
-    }
-  )
-};
+// You must add your own API key here from Clarifai. 
+const app = new Clarifai.App({
+    apiKey: '4cd4379a9d5f4d0d83e258446c340baa'
+});
 
 // const handleApiCall = (req, res) => {
-//   app.models
-//     .predict('53e1df302c079b3db8a0a36033ed2d15', req.body.input)
-//     .then(data => {
-//       res.json(data);
-//     })
-//     .catch(err => res.status(400).json('unable to work with API'))
-// }
+//     console.log('what');
+//     stub.PostModelOutputs(
+//     {
+//         // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+//         model_id: "6dc7e46bc9124c5c8824be4822abe105",
+//         inputs: [{data: {image: {url: req.body.input}}}]
+//     },
+//     metadata,
+//     (err, response) => {
+//         if (err) {
+//             console.log("Error: " + err);
+//             return;
+//         }
+//         if (response.status.code !== 10000) {
+//             console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+//             return;
+//         }
+//         console.log("Predicted concepts, with confidence values:")
+//         for (const c of response.outputs[0].data.concepts) {
+//             console.log(c.name + ": " + c.value);
+//         }
+//         res.json(response);
+//     }
+//   )
+// };
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict('53e1df302c079b3db8a0a36033ed2d15', req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
 
 const handleImage = (req, res, db)=> {
     const { id } = req.body;
